c7039eea3c83a841753df0ef8ced5fed49efbb67
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 14:22:15 -0600

    Animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -41,7 +41,7 @@ export const Nav = () => {
         </ul>
       </div>
       {/*Cart button*/}
-      <div className="fixed left-4 bottom-4 lg:static lg:mr-8">
+      <div className="btn-press-anim fixed left-4 bottom-4 lg:static lg:mr-8">
         <div className="flex-center cursor-pointer h-12 w-12 rounded-full bg-white shadow-md">
           <TbShoppingBag />
         </div>
--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -1,13 +1,14 @@
 import nike1 from "../assets/n1-min.png";
 import { Select } from "./Select";
 import { QTY, SIZES } from "../constant";
+
 export const ShoeDetail = () => {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
       {/* Shoe image */}
       <div className="flex-1 lg:-mt-32 lg:ml-28">
         <div className="flex-center h-full bg-gradient-to-br from-[#F637CF] via-[#E3D876] to-[#4DD4C6]">
-          <img src={nike1} alt="nike1" />
+          <img className="animate-float" src={nike1} alt="nike1" />
         </div>
       </div>
       <div className="flex-1 space-y-6">
@@ -25,7 +26,7 @@ export const ShoeDetail = () => {
         </div>
         {/* Shoe buttons and links */}
         <div className="space-x-10">
-          <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
+          <button className="btn-press-anim h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to bag
           </button>
           <a
