9e138ed9668b36b19f182d0d3154d0cafa0a5649
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 10:56:17 -0600

    Mobile nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import NikeLogo from "../assets/nike-logo.svg?react";
 
+const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
+
 export const Nav = () => {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="flex flex-wrap justify-between items-center">
       {/* Logo */}
       <a href="#">
         <NikeLogo className="h-20 w-20" />
@@ -12,6 +14,21 @@ export const Nav = () => {
       <button className="hover:bg-gray-100 rounded-lg p-2 focus:ring-2 focus:ring-gray-200">
         <RxHamburgerMenu size={25} />
       </button>
+      {/* Menu list */}
+      <div className="w-fll">
+        <ul className="bg-gray-50 text-lg border border-gray-50 rounded-lg p-4">
+          {ROUTES.map((route, index) => (
+            <li
+              className={`rounded py-2 px-3 cursor-pointer ${
+                index == 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"
+              }`}
+              key={route}
+            >
+              {route}
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
