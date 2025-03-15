f688a1f8bbda8fb9b3225882eb9daf43648acefe
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 14:44:54 -0600

    Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Nav } from "./components/Nav";
 import { ShoeDetail } from "./components/ShoeDetail";
+import { Card } from "./components/Card";
+import { SHOES } from "./constant";
 
 export const App = () => {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      <Nav />
-      <ShoeDetail />
+      {/* <Nav />
+      <ShoeDetail /> */}
+      <Card item={SHOES[0]} />
     </div>
   );
 };
