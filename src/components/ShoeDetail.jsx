c272a522e71db1c2dc927455d35a175b5d7cb115
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 13:04:22 -0600

    Select

--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -1,5 +1,6 @@
 import nike1 from "../assets/n1-min.png";
-
+import { Select } from "./Select";
+import { QTY, SIZES } from "../constant";
 export const ShoeDetail = () => {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
@@ -17,7 +18,11 @@ export const ShoeDetail = () => {
             "The Nike Air Max 270 is a lifestyle shoe that's sure to turn heads with its vibrant color gradient."
           }
         </div>
-        <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+        <div className="flex space-x-6">
+          <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+          <Select title={"QTY"} options={QTY} />
+          <Select title={"SIZE"} options={SIZES} />
+        </div>
         {/* Shoe buttons and links */}
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
