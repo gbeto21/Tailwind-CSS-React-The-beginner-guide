6a2d3deacd870f774ed9f3cd86866da0893afe26
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 12:18:20 -0600

    Gradient and fonts

--- src/components/ShoeDetail.jsx
+++ src/components/ShoeDetail.jsx
@@ -3,10 +3,14 @@ import nike1 from "../assets/n1-min.png";
 export const ShoeDetail = () => {
   return (
     <div className="flex flex-col lg:flex-row-reverse space-y-4">
+      {/* Shoe image */}
       <div className="flex-1 ">
-        <img src={nike1} alt="nike1" />
+        <div className="bg-gradient-to-br from-[#F637CF] via-[#E3D876] to-[#4DD4C6]">
+          <img src={nike1} alt="nike1" />
+        </div>
       </div>
       <div className="flex-1 space-y-6">
+        {/* Shoe text details */}
         <div className="text-5xl font-black md:text-9xl">Nike Air max 270</div>
         <div className="font-medium md:text-xl">
           {
@@ -14,6 +18,7 @@ export const ShoeDetail = () => {
           }
         </div>
         <div className="text-3xl font-extrabold md:text-6xl">100 $</div>
+        {/* Shoe buttons and links */}
         <div className="space-x-10">
           <button className="h-14 w-44 bg-black text-white hover:bg-gray-900 active:bg-gray-700">
             Add to bag
