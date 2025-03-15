615320f4e336b9e174ae5304e49b3b10a69c7ee5
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 16:27:25 -0600

    Card grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { Nav } from "./components/Nav";
+import { NewArrivalSection } from "./components/NewArrivalSection";
 import { ShoeDetail } from "./components/ShoeDetail";
-import { Card } from "./components/Card";
 import { SHOES } from "./constant";
 
 export const App = () => {
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
-      {/* <Nav />
-      <ShoeDetail /> */}
-      <Card item={SHOES[0]} />
+      <Nav />
+      <ShoeDetail />
+      <NewArrivalSection items={SHOES} />
     </div>
   );
 };
