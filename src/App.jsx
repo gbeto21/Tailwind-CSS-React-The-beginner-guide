0ba5d4da55e343a91e1e76488afec68cc51e66c1
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-15 21:00:26 -0600

    Dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { BiMoon } from "react-icons/bi";
+import { BiSun } from "react-icons/bi";
+import { useEffect, useState } from "react";
 import { Nav } from "./components/Nav";
 import { NewArrivalSection } from "./components/NewArrivalSection";
 import { ShoeDetail } from "./components/ShoeDetail";
@@ -13,6 +15,21 @@ const FAKE_CAR = SHOES.map((shoe) => {
 export const App = () => {
   const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+    if (isDarkMode === "true") {
+      window.document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark")
+    );
+  };
+
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClick={() => setIsSideBarOpen(true)} />
@@ -25,6 +42,15 @@ export const App = () => {
         <h2 className="text-2xl font-bold mb-10">Cart</h2>
         <Car carItems={FAKE_CAR} />
       </Sidebar>
+      <div className="fixed bottom-4 right-4">
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 rounded-full px-4 py-2 text-white dark:bg-white dark:text-night shadow-lg"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 };
